541d758974a199724f41f4fc81baa5221dc815db
Johannes-Pabst <116497020+Johannes-Pabst@users.noreply.github.com>
2025-08-27 14:06:06 +0200

    Delete script_dependencies directory

--- script_dependencies/vaultHuntersAfterRestart.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start taskmgr
-start "" "https://www.youtube.com/watch?v=oTBRAme_sL0&list=PLU2851hDb3SE6S9YJFY6n1B4t_Qv26f1m"
-start "" "C:\Users\johan\Desktop\Vault Hunters Official Modpack (3rd Ed)"
